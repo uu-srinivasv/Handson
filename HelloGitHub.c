785cb7818960b09f7f9399ef260d2d844bee95d3
srinivasv <srinivasv@uurmi.com>
2017-02-14 12:05:27 +0530

    printunderline

--- HelloGitHub.c
+++ HelloGitHub.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
+//File Header: Fundction used to print welcome message
+
 void main(void)
 {
     Printf("Welcome to GitHub World\n");
+    Printf("*************************\n");
 }
